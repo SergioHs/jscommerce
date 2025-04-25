f8ecf51708e8f5cfe57706156131cc67f82ff4a4
SergioHs <shs_28@hotmail.com>
2025-04-24 21:57:42 -0300

    Update app dependencies and index

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./src/routes/users.routes');
 const productRoutes = require('./src/routes/products.routes');
+const authRoutes = require('./src/routes/auth.routes')
+const { verifyToken } = require('./src/middlewares/auth.middleware');
 
 app.use(express.json());
+app.use('/auth', authRoutes)
 app.use('/users', userRoutes);
-app.use('/products', productRoutes);
+app.use('/products', verifyToken, productRoutes);
 
 const port = 3000;
 app.listen(port, () => console.log("Servidor iniciado em: http://localhost:3000"));
