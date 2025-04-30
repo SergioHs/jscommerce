0f8c21173a2037fdd79664f52a6c564463109dd6
SergioHs <shs_28@hotmail.com>
2025-04-29 21:19:34 -0300

    Create new middlewares

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRoutes = require('./src/routes/users.routes');
 const productRoutes = require('./src/routes/products.routes');
-const authRoutes = require('./src/routes/auth.routes')
+const authRoutes = require('./src/routes/auth.routes');
+
 const { verifyToken } = require('./src/middlewares/auth.middleware');
+const { logRequest } = require('./src/middlewares/log.middleware');
+const { limiter } = require('./src/middlewares/limiter.middleware');
 
 app.use(express.json());
-app.use('/auth', authRoutes)
-app.use('/users', userRoutes);
+app.use(logRequest);
+app.use(limiter)
+
+//Public routes
+app.use('/auth', authRoutes);
+
+//Private routes
+app.use('/users', verifyToken, userRoutes);
 app.use('/products', verifyToken, productRoutes);
 
-const port = 3000;
-app.listen(port, () => console.log("Servidor iniciado em: http://localhost:3000"));
+const port = process.env.PORT;
+app.listen(port, () => console.log("Servidor iniciado em: http://localhost:"+port));
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,19 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
 const { SECRET } = require('../services/auth.service');
+
+const logFilePath = path.join(__dirname, '../data/auth.log');
+
+const logUser = (user) => {
+    const logEntry = `[${new Date().toISOString()}] User ID: ${user.id}, Username: ${user.username}\n`;
+
+    fs.appendFile(logFilePath, logEntry, (error) => {
+        if(error){ 
+           console.error("Erro ao gravar o log de autenticação: " + error.message); 
+        }
+    })
+}
 
 const verifyToken = (request, response, next) => {
 
@@ -12,6 +26,7 @@ const verifyToken = (request, response, next) => {
 
     try {
         const decoded = jwt.verify(token, SECRET);
+        logUser(decoded);
         request.user = decoded;
         next();
     } catch (error) {
